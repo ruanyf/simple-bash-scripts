a3de8cb0c636e3a115157652772a8a801d993a13
Jim Dersch <dncrjim@gmail.com>
2020-10-04 14:02:29 -0400

    Update fibonacci.sh to reflect description
    The description says this tests if a number is a fibonacci, however, the script as written simply prints the sequence. Updated to reflect readme description. I hope you like it.

--- scripts/fibonacci.sh
+++ scripts/fibonacci.sh
@@ -1,11 +1,27 @@
 #!/usr/bin/env bash
 
-x=0; y=1; i=2
-while true; do
-	i=$((i + 1))
+echo -n "Enter the integer you would like to test. If you enter 0, I will print the sequence: "
+read a
+
+x=0; y=1
+
+if [[ $a -eq 0 ]]
+  then
+    echo -n "$x "; sleep .5
+    echo -n "$y "; sleep .5
+    while true; do
 	z=$((x + y))
 	echo -n "$z "
 	x=$y
 	y=$z
 	sleep .5
-done
+    done 
+  else
+    while [[ a -ge y ]]; do
+  	if [[ $a -eq $y ]] ; then echo "$a is a fibonacci number!" ; exit 0 ; fi
+	z=$((x + y))
+	x=$y
+	y=$z
+    done
+    echo "$a is not a fibonacci number, sorry."
+fi
