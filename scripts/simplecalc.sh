a00a25b15a68e5e07f44def9dd41a406a9b2f7f2
ngokjeun <n.jeunw@gmail.com>
2022-11-07 17:31:16 +0800

    fixed bug in simplecalc.sh where continue would not ask for new input values

--- scripts/simplecalc.sh
+++ scripts/simplecalc.sh
@@ -3,10 +3,6 @@ clear
 sum=0
 i="y"
 
-echo " Enter one no."
-read n1
-echo "Enter second no."
-read n2
 while [ $i = "y" ]; do
 	echo "1.Addition"
 	echo "2.Subtraction"
@@ -16,18 +12,34 @@ while [ $i = "y" ]; do
 	read ch
 	case $ch in
 	1)
+		echo " Enter one no."
+		read n1
+		echo "Enter second no."
+		read n2
 		sum=$(expr $n1 + $n2)
 		echo "Sum ="$sum
 		;;
 	2)
+		echo " Enter one no."
+		read n1
+		echo "Enter second no."
+		read n2
 		sum=$(expr $n1 - $n2)
 		echo "Sub = "$sum
 		;;
 	3)
+		echo " Enter one no."
+		read n1
+		echo "Enter second no."
+		read n2
 		sum=$(expr $n1 \* $n2)
 		echo "Mul = "$sum
 		;;
 	4)
+		echo " Enter one no."
+		read n1
+		echo "Enter second no."
+		read n2
 		sum=$(expr $n1 / $n2)
 		echo "Div = "$sum
 		;;
